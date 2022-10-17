7c4e5b994619ed615062e8f77114849b7cac985c
Matthew Festa <festa.matthew@me.com>
2022-10-16 20:50:02 -0400

    added css for banner

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,10 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+=
+src/Banner.js,f/a/fad8cc0e9199207d8befc7e3a552d469e8dcfe8e
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+:
+
+src/Row.js,4/f/4f3ab2f13abff062ff9753994ee42dbc4ab1d05f
--- src/Banner.js
+++ src/Banner.js
@@ -2,37 +2,39 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import requests from './requests';
 
+const baseURL = "https://image.tmdb.org/t/p/original/";
+
 function Banner() {
     const [movie, setMovie] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
             const request = await axios.get(requests.fetchNetflixOriginals);
+            console.log(request);
             setMovie(
                 request.data.results[
                 Math.floor(Math.random() * request.data.results.length - 1)
                 ]
             );
             // Math.floor(Math.random() * request.data.results.length -1)
-        return request;
+            return request;
         }
         fetchData();
-    },[]);
+    }, []);
 
     console.log(movie);
   return (
-    <header class="banner"
+    <header className="banner"
         style={{
             backgroundSize: "cover",
-            backgroundImage: `url(
-                "https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"
-            )`,
+            backgroundImage: `url(${baseURL}${movie?.backdrop_path}"
+        )`,
             backgroundPosition: "center center",
         }}
     > 
         <div className="banner__contents">
             {/* Title */}
-            <h1>
+            <h1 className="banner__title">
                 {/* Catch all cases of name/title */}
                 {movie?.title || movie?.name || movie?.original_name}
             </h1>
